d7513e2864ee7e7c94a276d5b4b4c36c492721ed
alessandro.valentini.90 <alessandro.valentini.90@gmail.com>
2011-11-10 18:32:57 +0000

    Funzione welcome da terminare con funzioni database e log4j Funzione richiamp da terminare con funzioni database, log4j e aggiungere conversione PDF e annulla Login parzialmente implementato Registrati da aggiungere captcha Aggiunto paziente (aggiungere medico)
    Aggiunto DB manager loris

---
+++ CamelVaccination/src/java/dbManagement/dbManager.java
@@ -0,0 +1,105 @@
+package dbManagement;
+
+import com.mysql.jdbc.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author loris.dallago
+ */
+public class dbManager{
+    private dbConnector dbConn;
+
+    public dbManager() {
+        dbConn = new dbConnector();
+    }
+
+    public void releaseConnection(){
+        dbConn.closeConnection();
+    }
+
+    public ResultSet getPatientVaccinations(int patientID){
+        ResultSet res = null;
+        try {
+            String command;
+            command = "SELECT doctor_id, vaccination_date FROM vaccinations WHERE patient_id = " + patientID;
+            ResultSet set1 = dbConn.executeQuery(command);
+            command = "SELECT name, surname FROM doctors WHERE doctor_id = " + set1.getString("doctor_id");
+            ResultSet set2 = dbConn.executeQuery(command);
+            command = "SELECT name, surname, vaccination_date FROM " + set1.getCursorName() + " JOIN " + set2.getCursorName();
+            res = dbConn.executeQuery(command);
+        } catch (SQLException ex) {
+            Logger.getLogger(dbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+    public ResultSet getDoctorVaccinations(int doctorID) {
+        ResultSet res = null;
+        try {
+            String command;
+            command = "SELECT patient_id, vaccination_date FROM vaccinations WHERE doctor_id = " + doctorID;
+            ResultSet set1 = dbConn.executeQuery(command);
+            command = "SELECT name, surname FROM patients WHERE patient_id = " + set1.getString("patient_id");
+            ResultSet set2 = dbConn.executeQuery(command);
+            command = "SELECT name, surname, vaccination_date FROM " + set1.getCursorName() + " JOIN " + set2.getCursorName();
+            res = dbConn.executeQuery(command);
+            return res;
+        } catch (SQLException ex) {
+            Logger.getLogger(dbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+    private String getDBtime(){
+        String res = null;
+        try {
+            String command = "SELECT NOW()";
+            ResultSet set = dbConn.executeQuery(command);
+            res = set.getString(1);
+        } catch (SQLException ex) {
+            Logger.getLogger(dbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+    private String getDBdiffTime(String sec) {
+        String res = null;
+        try {
+            String command = "SELECT NOW() - INTERVAL " + sec + " SECOND";
+            ResultSet set = dbConn.executeQuery(command);
+            res = set.getString(0);
+        } catch (SQLException ex) {
+            Logger.getLogger(dbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+    public ResultSet getPreviousVaccinationsPatients(int doctorID, String sec){
+        ResultSet res = null;
+        try {
+            String command;
+            command = "SELECT patient_id FROM vaccinations WHERE doctor_id = " + doctorID +
+                    " AND vaccination_date <= " + getDBdiffTime(sec);
+            ResultSet set1 = dbConn.executeQuery(command);
+            command = "SELECT * FROM patients WHERE patient_id = " + set1.getString("patient_id");
+            res = dbConn.executeQuery(command);
+        } catch (SQLException ex) {
+            Logger.getLogger(dbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+    public ResultSet userMatches(String userName, String pwd, boolean isDoctor){
+        ResultSet res;
+        String userKind = ((isDoctor) ? "doctor" : "patient");
+        String command = "SELECT * FROM " + userKind + "s WHERE "
+                + "username = " + userName + " AND "
+                + "password = " + pwd + " AND "
+                + ((isDoctor) ? " TRUE" : "registered");
+        res = dbConn.executeQuery(command);        
+        return res;
+    }
+}
