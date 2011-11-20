7cebce3a62b79e8b8ab9fe820375483f74bb8ad3
sid_aa@hotmail.it <sid_aa@hotmail.it@c051d98c-acd0-d992-6144-d018d47e1154>
2011-11-20 17:08:11 +0000

    Added redirections in the case of illegal acces by users

--- trunk/CamelVaccination/src/java/filtersManagement/DoctorFilter.java
+++ trunk/CamelVaccination/src/java/filtersManagement/DoctorFilter.java
@@ -55,6 +55,7 @@ public class DoctorFilter implements Filter {
                 String errMsg = "Un visitatore ha cercato di accedere " + 
                         "indebitamente alla pagina" + webPage;
                 Log4k.warn(DoctorFilter.class.getName(), errMsg);
+                response.sendRedirect("login.jsp");
             } else {
                 if (!user.getIsDoctor()) {
                     String errMsg = "L'utente " + user.getUsername()
@@ -62,6 +63,7 @@ public class DoctorFilter implements Filter {
                             + ") ha cercato di accedere indebitamente " +
                             "alla pagina" + webPage;
                     Log4k.warn(DoctorFilter.class.getName(), errMsg);
+                    response.sendRedirect("/Welcome");
                 } else {
                     chain.doFilter(inRequest, inResponse);
                 }
