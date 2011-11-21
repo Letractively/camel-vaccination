3765b23342ef5e6b967dc10b4bc0283a32f587d1
sid_aa <sid_aa@hotmail.it>
2011-11-21 16:17:37 +0000

    added a function to update the vaccinations table on a new regitration, added a function to retrieve information about a doctor, given its id, added a function to visuaize vaccinated people after a certain date + general code reformat

--- CamelVaccination/src/java/dbManagement/dbManager.java
+++ CamelVaccination/src/java/dbManagement/dbManager.java
@@ -31,6 +31,19 @@ public class dbManager{
         return res;
     }
 
+    public ResultSet getDoctor(int doctorID){
+        ResultSet res = null;
+        try {
+            String command;
+            command = "SELECT * FROM doctors WHERE id = "+ doctorID;
+            res = dbConn.executeQuery(command);
+        } catch (Exception ex) {
+            Log4k.error(dbManager.class.getName(), ex.getMessage());
+        }
+        return res;
+    }
+
+    
     public ResultSet getPatientVaccinations(int patientID){
         ResultSet res = null;
         try {
@@ -60,7 +73,7 @@ public class dbManager{
         return res;
     }
 
-    private String getDBtime(){
+    public String getDBtime(){
         String res = null;
         try {
             String command = "SELECT NOW() AS N";
@@ -90,16 +103,33 @@ public class dbManager{
         ResultSet res = null;
         try {
             String command;
-            /* Select the last vaccination of each vaccinated patient */
-            command = "SELECT * FROM " +
-                    "(SELECT P.*, V.doctor_id FROM " +
-                    "(SELECT * FROM patients LEFT JOIN " +
-                    "(SELECT DISTINCT MAX(vaccination_date) AS 'max', patient_id AS 'pid' FROM vaccinations GROUP BY patient_id) AS X "+
-                    "ON patients.id = X.pid) AS P "+
-                    "LEFT JOIN vaccinations AS V "+
-                    "ON P.id = V.patient_id AND P.max = V.vaccination_date) AS Z "+
-                    "WHERE (max <= '" + getDBdiffTime(sec) + "' OR max IS NULL) ";
-            
+            /* Select the last vaccination of each vaccinated patient before sec seconds ago */
+            String lastVaccinations = "SELECT DISTINCT MAX(vaccination_date) AS 'max', patient_id AS 'pid' FROM vaccinations GROUP BY patient_id";
+            String JT1 = "SELECT * " +
+                    "FROM patients LEFT JOIN (" + lastVaccinations + ") AS last_vaccinations " +
+                    "ON patients.id = last_vaccinations.pid";
+            String JT2 = "SELECT JT1.*, V.doctor_id " +
+                    "FROM (" + JT1 + ") AS JT1 LEFT JOIN vaccinations AS V " +
+                    "ON JT1.id = V.patient_id AND JT1.max = V.vaccination_date";
+            command = "SELECT * " +
+                    "FROM (" + JT2 + ") AS JT2 " +
+                    "WHERE (max <= '" + getDBdiffTime(sec) + "' OR max IS NULL)";            
+            System.out.println(command);
+            res = dbConn.executeQuery(command);
+        } catch (Exception ex) {
+            Log4k.error(dbManager.class.getName(), ex.getMessage());
+        }
+        return res;
+    }
+    
+        public ResultSet getFollowingVaccinationsPatients(int sec){
+        ResultSet res = null;
+        try {
+            String command;
+            /* Select the last vaccination of each vaccinated patient after sec seconds ago*/            
+            String lastVaccinations = "SELECT DISTINCT MAX(vaccination_date) AS 'vaccination_date', patient_id FROM vaccinations GROUP BY patient_id";
+            command = "SELECT * FROM (" + lastVaccinations + ") AS last_vaccinations" +
+                    " WHERE vacciantion_date >= '" + getDBdiffTime(sec) + "'";            
             System.out.println(command);
             res = dbConn.executeQuery(command);
         } catch (Exception ex) {
@@ -113,6 +143,18 @@ public class dbManager{
             String command;
             command = "INSERT INTO vaccinations(patient_id, doctor_id, vaccination_date)" + 
                     " VALUES (" + patient_id + ", " + doctor_id + ", " + "(SELECT NOW())" + ")";
+            dbConn.executeStatement(command);
+        } catch (Exception ex) {
+            Log4k.error(dbManager.class.getName(), ex.getMessage());
+        }
+    }
+    
+    public void doRegister(int patient_id, String new_pwd){
+        try {
+            String command;
+            command = "UPDATE patients" + 
+                    " SET password = " + new_pwd + ", registered = TRUE" +
+                    " WHERE patient_id = " + patient_id;
             dbConn.executeStatement(command);
         } catch (Exception ex) {
             Log4k.error(dbManager.class.getName(), ex.getMessage());
